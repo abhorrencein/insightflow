fde79fd68bd2a164b1edd7976df861d65bc9da06
abhorrencein <raj22anand@gmail.com>
2025-10-04 05:27:09 +0530

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter auto selects the appropriate adapter based on environment
     adapter: adapter()
   }
 };
